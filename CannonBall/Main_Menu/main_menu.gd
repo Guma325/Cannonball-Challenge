124534a9c8257426bfa08e533c88d08a73a97ebb
Luis Campregher <luis.campregher@hotmail.com>
2023-12-01 22:57:34 -0300

    Delete CannonBall directory

--- CannonBall/Main_Menu/main_menu.gd
+++
@@ -1,8 +0,0 @@
-class_name MainMenu
-extends Control
-
-
-@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
-@onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/Quit_Button as Button
-
-@export var start_level = preload("res://world.tscn")
